be444979e58f8712dfcccac6e47b81ff91a086f1
plankatron <fleeky@gmail.com>
2015-08-13 23:01:05 -0700

    arm working more

--- blender/keyboard.py
+++ blender/keyboard.py
@@ -3,6 +3,7 @@ import math
 from math import *
 import mathutils
 import serial
+import struct
 
 def keydown(key):
     return bge.logic.keyboard.events[key] == bge.logic.KX_INPUT_ACTIVE
@@ -13,36 +14,53 @@ arduino = None
 arduino = serial.Serial('/dev/ttyACM0', 115200)
 #arduino = serial.Serial('COM20', 115200)
 
-Inc = .1
+Inc = .01
 Servos = {}
 
 class Servo:
-    """angles are stored in radians but converted to degrees when sent to uC"""
-    def __init__(self, id, initial, min, max):
+    """
+    - id is the servo ID for the uC
+    - name is the blender bone name
+    Angles are stored in blender space, and in radians.
+    Servos accept degrees.
+    TODO clarify types of transformations between blender and servo space.
+    """
+    def __init__(self, id, name, initial, min, max, servoNegate=False, servoFlip=False):
         self.id = id
+        self.name = name
         self.pos = initial
         self.min = min
         self.max = max
+        self.servoNegate = servoNegate
+        self.servoFlip = servoFlip
     
-    def new(id, initial, min, max):
-        Servos[id] = Servo(id, initial, min, max)
+    def new(id, name, initial, min, max, servoNegate=False, servoFlip=False):
+        Servos[id] = Servo(id, name, initial, min, max, servoNegate, servoFlip)
+        #Servos[id].arduinoWrite()   # move to initial position
 
-    def arduinoWrite(self):a
+    def arduinoWrite(self):
         degrees = int(self.pos * 180/pi)
-        print(self.id, ':', degrees, ' degrees', sep='')
+        print(self.id, ':', degrees, ' degrees in blender space', sep='')
+        
+        if self.servoNegate: degrees = -degrees
+        if self.servoFlip: degrees = 180 - degrees
+        if self.name == 'shoulder': degrees = 180 - (degrees + 90)
+        return
         if arduino:
             arduino.write(struct.pack('>B', self.id))
             arduino.write(struct.pack('>B', degrees))
             arduino.write(struct.pack('>B', ord('\n')))
 
 
-Servo.new(0, 0, 0, pi)
-Servo.new(1, 0, 0, pi)
-Servo.new(2, 0, 0, pi)
+Servo.new(0, 'shoulder',    0,    -pi/2,      pi/2)
+Servo.new(1, 'upperarm',    0,     -pi,    0,      servoNegate=True)
+Servo.new(2, 'forearm',     0,     0,      pi,     servoFlip=True)
 
 
 def moveBoneServo(name, id, axis, inc):
     servo = Servos[id]
+    #if servo.invert: inc = -inc
+    
     pos = servo.pos + inc
     pos = min(pos, servo.max)
     pos = max(pos, servo.min)
@@ -58,13 +76,18 @@ def moveBoneServo(name, id, axis, inc):
     ob.update()
     return pos
 
+# set initial positions in blender space
+moveBoneServo('shoulder', 0, 'y', 0)
+moveBoneServo('upperarm', 1, 'x', 0)
+moveBoneServo('forearm',  2, 'x', 0)
 
 def k():
+
     if keydown(bge.events.QKEY): angle = moveBoneServo('shoulder', 0, 'y', Inc)
     if keydown(bge.events.WKEY): angle = moveBoneServo('shoulder', 0, 'y', -Inc)
 
-    if keydown(bge.events.AKEY): angle = moveBoneServo('upperarm', 1, 'z', Inc)
-    if keydown(bge.events.SKEY): angle = moveBoneServo('upperarm', 1, 'z', -Inc)
+    if keydown(bge.events.AKEY): angle = moveBoneServo('upperarm', 1, 'x', Inc)
+    if keydown(bge.events.SKEY): angle = moveBoneServo('upperarm', 1, 'x', -Inc)
         
-    if keydown(bge.events.ZKEY): angle = moveBoneServo('forearm', 2, 'z', -Inc)
-    if keydown(bge.events.XKEY): angle = moveBoneServo('forearm', 2, 'z', Inc)
+    if keydown(bge.events.ZKEY): angle = moveBoneServo('forearm', 2, 'x', Inc)
+    if keydown(bge.events.XKEY): angle = moveBoneServo('forearm', 2, 'x', -Inc)
