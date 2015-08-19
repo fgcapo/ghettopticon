8ed0cdc06304f5faaf1d306370408b232bba23a2
fgcapo <fgcapo@gmail.com>
2015-08-18 19:49:15 -0700

    documentation; reset mouse internal position after spacebar

--- blender/blenderbot.py
+++ blender/blenderbot.py
@@ -29,43 +29,71 @@ def sendChairCmd(cmd, val=0):
 
 #########################################################################
 
+mousePos = [0, 0]
+mouseSensitivity = .1
+
+# keyboard subroutine for interactive wheelchair movement
+# control left and right motors independently, as in controlling a tank
+# each motor can move forward or backward
+# hit spacebar to stop motors immediately
 def k():
     # heartbeat character sent every frame;
     # uC will stop motion if this is not received after a short amount of time
     sendChairCmd('~')
 
+	# left motor increase/decrease speed
     if keydown(bge.events.EKEY): sendChairCmd('e')
     if keydown(bge.events.DKEY): sendChairCmd('d')
-    if keydown(bge.events.UKEY): sendChairCmd('u')
+    # right motor increase/decrease speed
+	if keydown(bge.events.UKEY): sendChairCmd('u')
     if keydown(bge.events.HKEY): sendChairCmd('h')
-    if keydown(bge.events.SPACEKEY): sendChairCmd(' ')
+	
+	# full stop
+	if keydown(bge.events.SPACEKEY):
+	    sendChairCmd(' ')
+		mousePos = [0, 0]	#reset internal mouse position so that further mouse movement starts at zero
 
 
-mousePos = [0, 0]
-mouseSensitivity = .1
- 
+# mouse tracking subroutine for interactive wheelchair movement
+# This routine maps mouse position to wheelchair velocity, which was the simplest way I could think of
+#  to control the chair. I believe mapping the mouse position to the chair position would require
+#  mathematical integration plus some planning, as the chair can only move forward and back.
+#
+# Moving the mouse forward from the starting location increases forward speed.
+# Moving backward from starting location increases backward speed.
+# Moving left or right of the starting location makes the char spin in place (?)
+# All other mouse positions are interpolations of the above, ex: forward-right makes a left turn
+#  (can invert this easily).
+# 
+# Use spacebar to stop. 
+# TODO test mouse after hitting spacebar
 def m(cont):
     ob = cont.owner
     mouse = cont.sensors["Mouse"]
     
+	# find center of screen
     h = render.getWindowHeight()//2
     w = render.getWindowWidth()//2
     
+	# calculate the amount mouse cursor moved from the center since last frame
     mouseDelta = [
         (mouse.position[0] - w) * mouseSensitivity,
         (h - mouse.position[1]) * mouseSensitivity]
 
+    # move mouse cursor back to center
     render.setMousePosition(w, h)
 
+	# update our secret internal mouse position
     mousePos[0] += mouseDelta[0]
     mousePos[1] += mouseDelta[1]
 
+	# move cube onscreen
     ob.localPosition.x = mousePos[0]
     ob.localPosition.y = mousePos[1]
 
     print(mousePos)
 
-    #rotate 45 degrees to motor impulses
+    #rotate 45 degrees to convert to motor impulses
     motorPos = [
         mousePos[0]*pi/2 - mousePos[1]*pi/2,
         mousePos[1]*pi/2 + mousePos[0]*pi/2,
