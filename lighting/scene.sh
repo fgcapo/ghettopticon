885c2304baf66d5a80f19511b7f5663e324b176f
Francesco <fgcapo@gmail.com>
2015-10-12 23:46:45 -0700

    fade and scenes

---
+++ lighting/scene.sh
@@ -0,0 +1,2 @@
+python3 scene3.py
+
--- lighting/scene3.py
+++ lighting/scene3.py
@@ -36,8 +36,31 @@ class OlaThread(threading.Thread):
 OLA = OlaThread()
 OLA.start()
 
+def frange(start, end=None, inc=None):
+    "A range function, that does accept float increments..."
+
+    if end == None:
+        end = start + 0.0
+        start = 0.0
+    else: start += 0.0 # force it to be a float
+
+    if inc == None:
+        inc = 1.0
+
+    count = int((end - start) / inc)
+    if start + count * inc != end:
+        # need to adjust the count.
+        # AFAIKT, it always comes up one short.
+        count += 1
+
+    L = [None,] * count
+    for i in range(count):
+        L[i] = start + i * inc
+
+    return L
+
 while 1:
-  line = input('save, load, or exit: ')
+  line = input('> save, load, fade, or exit: ')
   tokens = line.split()  
   if len(tokens) == 0: continue
 
@@ -45,11 +68,12 @@ while 1:
     OLA.exit()
     break
 
-  if len(tokens) < 2:
-    print('no filename')
-    continue
-
+  # save current OLA scene
   if tokens[0] == 'save':
+    if len(tokens) < 2:
+      print('no filename')
+      continue
+
     filename = line[4:].strip()
     data = str(list(OLA.data)) 
     #print(data)
@@ -59,41 +83,60 @@ while 1:
     except:
       print('Error saving file')
 
+  # load scene from local file
   elif tokens[0] == 'load':
-      filename = line[4:].strip()
-    #print(filename)
-#    try:
+    if len(tokens) < 2:
+      print('no filename')
+      continue
+
+    filename = line[4:].strip()
+    try:
       with open(filename) as f:
         text = f.readline()
-        #print(text)
+        print(text)
         channels = ast.literal_eval(text)
-        print(channels)
         OLA.client.SendDmx(1, channels) 
-#    except:
-#      print('Error loading file')
+    except:
+      print('Error loading file')
 
-
+  # fade from current scene to new scene
+  # usage:
+  # fade <optional time in seconds> <filename>
   elif tokens[0] == 'fade':
-      time = 5.0
-      timestep = .1
+    timestep = .05
+    if len(tokens) < 2:
+      print('usage: fade <optional time in seconds> <filename>')
+      continue
+
+    try:  
+      tok = tokens[1]
+      period = float(tok)
+      filename = line[line.find(tok) + len(tok):].strip()
+    except:
+      period = 5.0
       filename = line[4:].strip()
-    #print(filename)
-#    try:
+
+    try:      
       with open(filename) as f:
         text = f.readline()
-        #print(text)
-        current = ast.literal_eval(text)
-        #print(channels)
-        vel = list(channels)
-        target = list(OLA.data)
+        print(text)
+        target = ast.literal_eval(text)
+        current = list(OLA.data)
+        vel = [0] * len(target)
+
+        print('fading for', period, 'seconds...')
+
         for i in range(len(channels)):
-          vel[i] = (target[i] - current[i]) / time
+          vel[i] = (target[i] - current[i]) * (timestep / period)
 
-        for t in range(0, time, timestep):
-          current[i] += vel[i]
+        for t in frange(0, period, timestep):
+          for i in range(len(current)): current[i] += vel[i]
+          channels = [int(x) for x in current] 
+          OLA.client.SendDmx(1, channels) 
           time.sleep(timestep)
-       
-        OLA.client.SendDmx(1, current) 
-#    except:
-#      print('Error loading file')
+    except (OSError, IOError) as e:
+      print('Error loading ')
+    # TODO other exceptions having to do with the fade math
 
+  else:
+    print('Unknown command')
