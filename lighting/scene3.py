b429316e267a8eb4ec689e8b6023daaa62e01fb6
Francesco Capodieci <fgcapo@gmail.com>
2015-10-13 19:32:35 -0700

    broke out commands

--- lighting/scene3.py
+++ lighting/scene3.py
@@ -2,39 +2,37 @@ import sys
 import os
 import os.path
 import threading
-from ola.ClientWrapper import ClientWrapper
 import ast
 import time
+from ola.ClientWrapper import ClientWrapper
 
 class OlaThread(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.data = None
+  def __init__(self):
+    threading.Thread.__init__(self)
+    self.data = [0] * 512
 
-    def run(self):
-        def onData(data):
-            # TODO check on status before assigning data
-            self.data = data
-            #print(data)
+  def run(self):
+    def onData(data):
+        # TODO check on status before assigning data
+        self.data = data
+        #print(data)
 
-        self.wrapper = ClientWrapper()
-        self.client = self.wrapper.Client()
-        universe = 1
-        self.client.RegisterUniverse(universe, self.client.REGISTER, onData)
-        print('running OLA thread')
-        self.wrapper.Run()
-        # TODO catch exceptions
+    self.wrapper = ClientWrapper()
+    self.client = self.wrapper.Client()
+    universe = 1
+    self.client.RegisterUniverse(universe, self.client.REGISTER, onData)
+    print('running OLA thread')
+    self.wrapper.Run()
+    # TODO catch exceptions
 
-    def exit(self):
-        #try: self.wrapper.Terminate()
-        self.wrapper.Stop()
+  def exit(self):
+    self.wrapper.Stop()
 
-    def getData(self):
-        # return or print?
-        return self.data
+  def getData(self):
+    return self.data
 
 OLA = OlaThread()
-OLA.start()
+#OLA.start()
 
 def frange(start, end=None, inc=None):
     "A range function, that does accept float increments..."
@@ -59,84 +57,108 @@ def frange(start, end=None, inc=None):
 
     return L
 
-while 1:
-  line = input('> save, load, fade, or exit: ')
-  tokens = line.split()  
-  if len(tokens) == 0: continue
+def restAfterWord(word, line):
+  return line[line.find(word) + len(word):].strip()
 
-  if tokens[0] == 'exit':
-    OLA.exit()
-    break
+# save current OLA scene
+def cmdSave(tokens, line):
+  if len(tokens) < 2:
+    raise BaseException('no filename')
 
-  # save current OLA scene
-  if tokens[0] == 'save':
-    if len(tokens) < 2:
-      print('no filename')
-      continue
+  filename = restAfterWord(tokens[0], line)
+  data = str(list(OLA.getData())) 
+  #print(data)
+  try:
+    with open(filename, 'w') as f:
+      f.write(data)
+  except:
+    raise BaseException('Error saving file')
 
-    filename = line[4:].strip()
-    data = str(list(OLA.data)) 
-    #print(data)
-    try:
-      with open(filename, 'w') as f:
-        f.write(data)
-    except:
-      print('Error saving file')
+# load scene from local file
+def cmdLoad(tokens, line, run=True):
+  if len(tokens) < 2:
+    raise BaseException('no filename')
 
-  # load scene from local file
-  elif tokens[0] == 'load':
-    if len(tokens) < 2:
-      print('no filename')
-      continue
+  filename = restAfterWord(tokens[0], line)
+  print(filename)
+  try:
+    with open(filename) as f:
+      text = f.readline()
+      #print(text)
+      channels = ast.literal_eval(text)
 
-    filename = line[4:].strip()
-    try:
-      with open(filename) as f:
-        text = f.readline()
-        print(text)
-        channels = ast.literal_eval(text)
-        OLA.client.SendDmx(1, channels) 
-    except:
-      print('Error loading file')
+      if not run: return
+      OLA.client.SendDmx(1, channels) 
+  except OSError as e:
+    raise BaseException('Error loading file')
+  except:
+    raise BaseException('Error talking to OLA DMX server')
 
   # fade from current scene to new scene
   # usage:
   # fade <optional time in seconds> <filename>
-  elif tokens[0] == 'fade':
-    timestep = .05
-    if len(tokens) < 2:
-      print('usage: fade <optional time in seconds> <filename>')
-      continue
+def cmdFade(tokens, line, run=True):
+  timestep = .05
+  if len(tokens) < 2:
+    raise BaseException('usage: fade <optional time in seconds> <filename>')
 
-    try:  
-      tok = tokens[1]
-      period = float(tok)
-      filename = line[line.find(tok) + len(tok):].strip()
-    except:
-      period = 5.0
-      filename = line[4:].strip()
+  try: 
+    tok = tokens[1]
+    period = float(tok)
+    filename = restAfterWord(tok, line)
+  except:
+    period = 5.0
+    filename = restAfterWord(tokens[0], line)
 
-    try:      
-      with open(filename) as f:
-        text = f.readline()
-        print(text)
-        target = ast.literal_eval(text)
-        current = list(OLA.data)
-        vel = [0] * len(target)
+  try:      
+    with open(filename) as f:
+      text = f.readline()
+      #print(text)
+      target = ast.literal_eval(text)
+      current = list(OLA.data)
+      vel = [0] * len(target)
 
-        print('fading for', period, 'seconds...')
-
-        for i in range(len(channels)):
+      # calculate delta for each timestep
+      # -1 means don't change
+      for i in range(len(target)):
+        if target[i] >= 0:
           vel[i] = (target[i] - current[i]) * (timestep / period)
 
-        for t in frange(0, period, timestep):
-          for i in range(len(current)): current[i] += vel[i]
-          channels = [int(x) for x in current] 
-          OLA.client.SendDmx(1, channels) 
-          time.sleep(timestep)
-    except (OSError, IOError) as e:
-      print('Error loading ')
-    # TODO other exceptions having to do with the fade math
+      if not run: return
 
-  else:
-    print('Unknown command')
+      print('fading for', period, 'seconds... ', end='', flush=True)
+
+      for t in frange(0, period, timestep):
+        for i in range(len(current)): current[i] += vel[i]
+        channels = [int(x) for x in current] 
+        OLA.client.SendDmx(1, channels) 
+        time.sleep(timestep)
+
+      print('done')
+  except (OSError, IOError) as e:
+    raise BaseException('Error loading ')
+  except:
+    raise BaseException('Error talking to OLA DMX server')
+  # TODO other exceptions having to do with the fade math
+
+
+if __name__ == '__main__':
+  while 1:
+    line = input('> save, load, fade, or exit: ')
+    tokens = line.split()  
+    if len(tokens) == 0: continue
+    cmd = tokens[0]
+
+    if cmd == 'exit':
+      OLA.exit()
+      break
+
+    try:
+      if cmd == 'save':   cmdSave(tokens, line)
+      elif cmd == 'load': cmdLoad(tokens, line)
+      elif cmd == 'fade': cmdFade(tokens, line)
+      else: print('Unrecigbuzed command')
+    except BaseException as e:
+      print(e)
+
+
