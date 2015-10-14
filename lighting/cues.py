a379af5997353dab36d82ee0bf73c37734dcd805
Francesco Capodieci <fgcapo@gmail.com>
2015-10-14 12:32:45 -0700

    integrated cue system

---
+++ lighting/cues.py
@@ -0,0 +1,169 @@
+import time
+from getch import getch
+import scene3 as scene
+from scene3 import OLA, restAfterWord, Cue
+
+CuesFilename = 'cuesheet.txt'
+
+class Scene:
+  def __init__(self, name):
+    self.name = name
+    self.cues = []
+  def addCue(self, cue):
+    self.cues.append(cue)
+
+class CueEngine:
+  def __init__(self):
+    self.scenes = [Scene('Beginning')]
+    self.scenes[0].addCue(scene.CueLoad('load off'))
+    self.ixScene = 0  # current scene
+    self.ixCue = -1   # cue we just ran
+  
+  def thisScene(self): return self.scenes[self.ixScene]
+  def thisCue(self):   return self.thisScene().cues[self.ixCue]
+
+  def addScene(self, scene):
+    self.scenes.append(scene)
+
+  def getLoc(self):
+    return (self.ixScene, self.ixCue)
+
+  def onLastCue(self):
+    return 1+self.ixCue >= len(self.thisScene().cues)
+  def onLastScene(self):
+    return 1+self.ixScene >= len(self.scenes)
+  def onFirstScene(self):
+    return self.ixScene <= 0
+
+  def nextCue(self):
+    if self.onLastCue():
+      if self.onLastScene():
+        print(self.getLoc(), 'At End')
+        return
+      self.ixScene += 1
+      self.ixCue = 0
+      print(self.getLoc(), 'Scene:', self.thisScene().name)
+    else:
+      self.ixCue += 1
+
+    print(self.getLoc(), 'next cue:', self.thisCue().line.strip())
+    self.thisCue().run()
+
+  def prevCue(self):
+    if self.ixCue <= 0:
+      if self.onFirstScene():
+        print(self.getLoc(), 'At Beginning')
+        return
+      self.ixScene -= 1
+      self.ixCue = len(self.thisScene().cues) - 1
+      print(self.getLoc(), 'Scene:', self.thisScene().name)
+    else:
+      self.ixCue -= 1
+
+    print(self.getLoc(), 'previous cue:', self.thisCue().line.strip())
+    self.thisCue().run()
+
+  def nextScene(self): 
+    if self.onLastScene():
+      print(self.getLoc(), 'On Last Scene')
+      return
+    self.ixScene += 1
+    self.ixCue = -1
+    print(self.getLoc(), 'next Scene:', self.thisScene().name)
+
+  def prevScene(self):
+    if self.onFirstScene():
+      print(self.getLoc(), 'On First Scene')
+      return
+    self.ixScene -= 1
+    self.ixCue = -1
+    print(self.getLoc(), 'previous Scene:', self.thisScene().name)
+
+
+CueMgr = CueEngine()
+
+
+with open(CuesFilename) as f:
+  curScene = None
+  #groupStack = []
+  #def pushGroup(cmd, indent): groupStack.append((cmd, indent))
+  #def lastGroupIndent(): return groupStack[-1][1]
+  numSeqLeft = 0
+  curSeq = None
+
+  line = f.readline()
+  while line:
+    tokens = line.split()
+    if len(tokens): 
+        cmd = tokens[0]
+      #indent = line[:line.find(cmd)]
+      #while(len(indent) < len(lastGroupIndent)
+
+      #try:
+        if cmd == 'scene':
+          curScene = Scene(restAfterWord(cmd, line))
+          CueMgr.addScene(curScene)
+        elif cmd == 'seq':
+          curSeq = scene.CueSequence(line)
+          try:
+            token = tokens[1]
+            numSeqLeft = int(token)
+          except:
+            print('Error: seq needs to be followed by the number of cues')
+            #OLA.exit()
+            exit()
+          curScene.addCue(curSeq)
+
+        elif cmd == 'load':
+          cue = scene.CueLoad(line)
+          if not curSeq: curScene.addCue(cue)
+          else:
+            curSeq.addCue(cue)
+            numSeqLeft -= 1
+            if numSeqLeft == 0: curSeq = None
+          
+        elif cmd == 'fade':
+          cue = scene.CueFade(line)
+          if not curSeq: curScene.addCue(cue)
+          else:
+            curSeq.addCue(cue)
+            numSeqLeft -= 1
+            if numSeqLeft == 0: curSeq = None
+
+        else:
+          print('Error unrecognized command')
+          print('Text', line)
+
+      #except BaseException as e:
+      #  print(e)
+      #  print('Text', line)
+
+    line = f.readline()
+
+if __name__ == '__main__':
+  # wait for OLA client to connect
+  time.sleep(1)
+  print('z:     exit')
+  print('Space: next cue')
+  print('b:     previous cue')
+  print('>:     next scene')
+  print('<:     previous scene')
+  print('----------------------')
+  print('Press Space initially to black out lights:')
+#  scene.CueLoad('load blackout').run()
+
+  while 1:
+    ch = getch().lower()
+    if ch == 'z':
+      OLA.exit()
+      break
+    elif ch == ' ':
+      CueMgr.nextCue()
+    elif ch == 'b' or ch == 'B':
+      CueMgr.prevCue()
+    elif ch == '.' or ch == '>':
+      CueMgr.nextScene()
+    elif ch == ',' or ch == '<':
+      CueMgr.prevScene()
+
+
--- lighting/scene3.py
+++ lighting/scene3.py
@@ -21,7 +21,7 @@ class OlaThread(threading.Thread):
     self.client = self.wrapper.Client()
     universe = 1
     self.client.RegisterUniverse(universe, self.client.REGISTER, onData)
-    print('running OLA thread')
+    print('running OLA thread...')
     self.wrapper.Run()
     # TODO catch exceptions
 
@@ -32,7 +32,7 @@ class OlaThread(threading.Thread):
     return self.data
 
 OLA = OlaThread()
-#OLA.start()
+OLA.start()
 
 def frange(start, end=None, inc=None):
     "A range function, that does accept float increments..."
@@ -60,6 +60,114 @@ def frange(start, end=None, inc=None):
 def restAfterWord(word, line):
   return line[line.find(word) + len(word):].strip()
 
+########################################################
+# cue commands
+
+class Cue:
+  def __init__(self, line):
+    self.line = line
+
+  def run(self):
+    print('empty cue')
+
+class CueSequence(Cue):
+  def __init__(self, line):
+    Cue.__init__(self, line)
+    self.cues = []
+  def addCue(self, cue):
+    self.cues.append(cue)
+  def run(self):
+    for cue in self.cues:
+      print ('-', cue.line.strip())
+      cue.run()
+
+# load scene from local file
+class CueLoad(Cue):
+  def __init__(self, line):
+    Cue.__init__(self, line)
+
+    tokens = line.split()
+    if len(tokens) < 2:
+      raise BaseException('no filename')
+
+    filename = restAfterWord(tokens[0], line)
+    #print(filename)
+    try:
+      with open(filename) as f:
+        text = f.readline()
+        #print(text)
+        self.target = ast.literal_eval(text)
+    except OSError as e:
+      raise BaseException('Error loading file: ' + str(e))
+
+  def run(self):
+    try:
+      OLA.client.SendDmx(1, self.target)
+    except:
+      raise BaseException('Error talking to OLA DMX server')
+
+    
+  # fade from current scene to new scene
+  # usage:
+  # fade <optional time in seconds> <filename>
+class CueFade(Cue):
+  def __init__(self, line):
+    Cue.__init__(self, line)
+
+    tokens = line.split()
+    if len(tokens) < 2:
+      raise BaseException('usage: fade <optional time in seconds> <filename>')
+
+    try: 
+      tok = tokens[1]
+      self.period = float(tok)
+      filename = restAfterWord(tok, line)
+    except:
+      self.period = 5.0
+      filename = restAfterWord(tokens[0], line)
+
+    try:      
+      with open(filename) as f:
+        text = f.readline()
+        #print(text)
+        self.target = ast.literal_eval(text)
+    except (OSError, IOError) as e:
+      raise BaseException('Error loading file: ' + str(e))
+ 
+  def run(self):
+    try:
+      timestep = .05
+      current = list(OLA.data)
+      vel = [0] * len(current)
+
+      # calculate delta for each timestep
+      # -1 means don't change
+      for i in range(len(self.target)):
+        if self.target[i] >= 0:
+          vel[i] = (self.target[i] - current[i]) * (timestep / self.period)
+
+      print('fading for', self.period, 'seconds............................. ', end='', flush=True)
+
+      for t in frange(0, self.period, timestep):
+        for i in range(len(current)): current[i] += vel[i]
+        channels = [int(x) for x in current] 
+        OLA.client.SendDmx(1, channels)
+        time.sleep(timestep)
+
+      print('DONE')
+    except:
+      raise BaseException('Error talking to OLA DMX server')
+    # TODO other exceptions having to do with the fade math
+
+
+# instantiate a class and run it
+def cmdCue(line, CueClass):
+  try:
+    cue = CueClass(line)
+    cue.run()
+  except BaseException as e:
+    print(e)
+
 # save current OLA scene
 def cmdSave(tokens, line):
   if len(tokens) < 2:
@@ -74,72 +182,6 @@ def cmdSave(tokens, line):
   except:
     raise BaseException('Error saving file')
 
-# load scene from local file
-def cmdLoad(tokens, line, run=True):
-  if len(tokens) < 2:
-    raise BaseException('no filename')
-
-  filename = restAfterWord(tokens[0], line)
-  print(filename)
-  try:
-    with open(filename) as f:
-      text = f.readline()
-      #print(text)
-      channels = ast.literal_eval(text)
-
-      if not run: return
-      OLA.client.SendDmx(1, channels) 
-  except OSError as e:
-    raise BaseException('Error loading file')
-  except:
-    raise BaseException('Error talking to OLA DMX server')
-
-  # fade from current scene to new scene
-  # usage:
-  # fade <optional time in seconds> <filename>
-def cmdFade(tokens, line, run=True):
-  timestep = .05
-  if len(tokens) < 2:
-    raise BaseException('usage: fade <optional time in seconds> <filename>')
-
-  try: 
-    tok = tokens[1]
-    period = float(tok)
-    filename = restAfterWord(tok, line)
-  except:
-    period = 5.0
-    filename = restAfterWord(tokens[0], line)
-
-  try:      
-    with open(filename) as f:
-      text = f.readline()
-      #print(text)
-      target = ast.literal_eval(text)
-      current = list(OLA.data)
-      vel = [0] * len(target)
-
-      # calculate delta for each timestep
-      # -1 means don't change
-      for i in range(len(target)):
-        if target[i] >= 0:
-          vel[i] = (target[i] - current[i]) * (timestep / period)
-
-      if not run: return
-
-      print('fading for', period, 'seconds... ', end='', flush=True)
-
-      for t in frange(0, period, timestep):
-        for i in range(len(current)): current[i] += vel[i]
-        channels = [int(x) for x in current] 
-        OLA.client.SendDmx(1, channels) 
-        time.sleep(timestep)
-
-      print('done')
-  except (OSError, IOError) as e:
-    raise BaseException('Error loading ')
-  except:
-    raise BaseException('Error talking to OLA DMX server')
-  # TODO other exceptions having to do with the fade math
 
 
 if __name__ == '__main__':
@@ -155,8 +197,8 @@ if __name__ == '__main__':
 
     try:
       if cmd == 'save':   cmdSave(tokens, line)
-      elif cmd == 'load': cmdLoad(tokens, line)
-      elif cmd == 'fade': cmdFade(tokens, line)
+      elif cmd == 'load': cmdCue(line, CueLoad)
+      elif cmd == 'fade': cmdCue(line, CueFade)
       else: print('Unrecigbuzed command')
     except BaseException as e:
       print(e)
