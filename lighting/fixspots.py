01d82ddee02a2a991dfdcd40782c1ec5ad579c1a
Francesco <fgcapo@gmail.com>
2015-10-23 17:50:48 -0700

    adjust track spot settings en masse

--- lighting/fixspots.py
+++ lighting/fixspots.py
@@ -1,3 +1,5 @@
+# script to modify track spot settings in all cue files in scenes/ to make them more amenable to manual control during show
+
 from trackspot import *
 import os, ast
 
@@ -7,28 +9,41 @@ spots = [
       TrackSpot(None, 193, 'g', 'b', 'v', 'n', 'h', 'f')]
 
 for filename in os.listdir('scenes'):
-  if not os.path.isfile(filename): continue
+#  if not os.path.isfile(filename): continue
+
+  json = dmx = None
 
   with open('scenes/' + filename, 'r') as f:
 #    try:
       text = f.read()
-      print(text)
-      continue
+      #print(text)
 
-
-      json = ast.literal_eval(f.read())
+      json = ast.literal_eval(text)
       dmx = None
       if isinstance(json, list): dmx = json
       elif isinstance(json, dict): dmx = json['DMX']
       else: raise BaseException('parse error')
 
-      print(dmx)
-      for spot in spots:
-        if dmx[spot.intensity] == 0 and (dmx[spot.strobe] > 0 or dmx[spot.speed] > 0):
-          print('file "' + filename + '" has weirdness on spot', spot.firstChannel)
+  changed = False
 
-        elif dmx[spot.intensity] > 0 and (dmx[spot.strobe] == 0 or dmx[spot.speed] == 0):
-          print('file "' + filename + '" has weirdness on spot', spot.firstChannel)
+  for spot in spots:
+    #if dmx[spot.intensity] == 0 and (dmx[spot.strobe] > 0 or dmx[spot.speed] > 0):
+    #  print('file "' + filename + '" has weirdness on spot', spot.firstChannel+1)
+
+    if dmx[spot.intensity] > 0 and (dmx[spot.strobe] != 255 or dmx[spot.speed] != 255):
+      print('file "' + filename + '" has weirdness on spot', spot.firstChannel+1)
+      changed = True
+      dmx[spot.strobe] = 255
+      dmx[spot.speed] = 255
+      dmx[spot.intensity] = 0
+
+  if changed: 
+    with open('scenes/' + filename, 'w') as f:
+      if dmx == json:
+        f.write(str(dmx))
+      else:
+        f.write(str(json))
+        f.write('\n') 
 
  #   except BaseException as e:
   #    print('Error reading file: "' + filename + '":', e)
